b53aaf39b8314f4a2f9bccd2c96acd037cb5912d
近藤　倫太 <tora.goraias@gmail.com>
2022-07-08 16:20:43 +0900

    wip

--- Assets/Kondo/Scripts/PlayerMove.cs
+++ Assets/Kondo/Scripts/PlayerMove.cs
@@ -55,9 +55,14 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+
+    //}
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
+                if (collision.gameObject.tag == "Ground")
         {
             _isGrounded = true;
         }
